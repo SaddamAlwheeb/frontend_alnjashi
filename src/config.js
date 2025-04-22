9d3233c2f25cc9e471b1f6bcf2a457f5fbd4a657
saddam <saddam@YSoftIT2016.net>
2025-04-22 11:29:28 +0300

    change log of the project

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const BASE_URL = 'http://localhost:5454/';
+// export const BASE_URL = 'http://alnajashib.mostaid.net/';
 export const GOOGLE_TRANSLATE_API_KEY = 'AIzaSyB7q7By8yeI9xb15Z0w0atMogpdLryjYs8';
